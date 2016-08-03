19f76499796dcbd21d24474a94cd3ae8b827d8d2
nkerman <nathaniel.kerman@yale.edu>
2016-08-03 10:28:28 -0400

    Wrote the basic README, added (BROKEN) fits compatability to make_plot.

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # teambadass
+
+
+
+This is a module for working with spectra files.
+
+
+
+
+
+__funkyfunction__
+
+* Reads in ascii data file to "table" list
+
+
+* It uses the matplotlib module to plot() wavelength (table[:,0]) against flux (table[:,1])
+
+__to use:__
+
+* add make_plot directory to your python path
+* Go into data directory
+* from funkyfunction import make_plot
+* make_plot('filename.extension')
+  * extension can definitely be ascii, maybe fits
--- make_plot/funkyfunction.py
+++ make_plot/funkyfunction.py
@@ -1,7 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from spectrum import Spectrum
 
 def make_plot(spectrumfile):
+	if spectrumfile[-4:] == 'fits':
+		spectrum = Spectrum(file =sys.argv[1])
+		fig, (ax) = p.subplots(1,1)
+		ax.plot(spectrum.data[spectrum.cols.names[0]], spectrum.data[spectrum.cols.names[1]])
+		ax.set_xlabel("Wavelength ("+spectrum.cols.units[0].lower()+")")
+		ax.set_ylabel("Flux ("+spectrum.cols.units[1].lower()+")")
 	table = np.loadtxt(spectrumfile, skiprows=1)
 	wav = table[:,0]
 	flux = table[:,1]
